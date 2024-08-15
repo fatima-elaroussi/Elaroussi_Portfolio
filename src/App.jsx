55abe6709b96321f6e6cdbd2ec20d35dbe1be41f
fatima-elarouss <elaroussifatima7@gmail.com>
2024-08-15 02:50:16 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  
+  return (
+    <>
+    <h6 className='text-3xl underline text-cyan-500'>Hello</h6>
+     
+    </>
+  )
+}
+
+export default App
